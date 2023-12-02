077f89cb14f53631bfbb1506c5f5c629caf58317
nekoffski <kacperczyz@protonmail.com>
2023-12-02 14:43:13 +0100

    1

--- solutions/1.cpp
+++ solutions/1.cpp
@@ -1,6 +1,55 @@
 #include <utils.hpp>
 
-int main() {
-    fmt::print("Hello aoc!\n");
+int main(int argc, char** argv) {
+    const auto input = readInput(argc, argv);
+
+    // 1
+    const auto output1 = std::accumulate(
+      input.begin(), input.end(), 0u,
+      [&](u32 rest, std::string line) -> u32 {
+          LOG("{}", line);
+          std::erase_if(line, [](char c) -> bool { return c < '0' || c > '9'; });
+          if (line.size() == 1)
+              line.append(std::string{ line[0] });
+          else
+              line = fmt::format("{}{}", line[0], line[line.size() - 1]);
+          LOG("{} + {}", rest, line);
+          if (line.size() == 0) return rest;
+          return rest + std::stoi(line);
+      }
+    );
+    LOG("Output1 = {}", output1);
+    // 2
+    std::unordered_map<std::string, u32> dict = {
+        {"one",    1},
+        { "two",   2},
+        { "three", 3},
+        { "four",  4},
+        { "five",  5},
+        { "six",   6},
+        { "seven", 7},
+        { "eight", 8},
+        { "nine",  9},
+    };
+    for (u32 i = 0; i < 10u; ++i) dict[std::to_string(i)] = i;
+
+    const auto output2 = std::accumulate(
+      input.begin(), input.end(), 0u,
+      [&](u32 rest, std::string line) -> u32 {
+          std::vector<std::pair<int, int>> records;
+
+          for (const auto& [s, d] : dict) {
+              if (u64 l = line.find(s); l != line.npos) records.push_back({ l, d });
+              if (u64 r = line.rfind(s); r != line.npos) records.push_back({ r, d });
+          }
+
+          const auto& [min, max] = std::minmax_element(
+            records.begin(), records.end(),
+            [](auto& lhs, auto& rhs) -> bool { return lhs.first <= rhs.first; }
+          );
+          return rest + (min->second * 10 + max->second);
+      }
+    );
+    LOG("Output2 = {}", output2);
     return 0;
 }
--- utils.hpp
+++ utils.hpp
@@ -1,5 +1,54 @@
 #pragma once
 
+#include <string>
 #include <vector>
+#include <ostream>
+#include <fstream>
+#include <sstream>
+#include <cstdint>
+#include <numeric>
+#include <ranges>
+#include <algorithm>
+#include <concepts>
 
 #include <fmt/core.h>
+
+using u64 = uint64_t;
+using u32 = uint32_t;
+using u16 = uint16_t;
+using u8  = uint8_t;
+
+template <typename T>
+requires std::is_integral_v<T>
+constexpr T charToIntegral(char c) { return static_cast<T>(c - '0'); }
+
+#define LOG(...)                 \
+    {                            \
+        fmt::print("$ - ");      \
+        fmt::print(__VA_ARGS__); \
+        fmt::print("\n");        \
+    }
+
+#define ASSERT(condition, ...)                        \
+    if (not(condition)) {                             \
+        fmt::print("ERROR: ");                        \
+        fmt::print(__VA_ARGS__);                      \
+        fmt::print("\n");                             \
+        throw std::runtime_error("Assertion failed"); \
+    }
+
+std::vector<std::string> readInput(int argc, char** argv) {
+    ASSERT(argc > 1, "Input file is required");
+    std::string inputFile = argv[1];
+    LOG("Input file - '{}'", inputFile);
+
+    std::ifstream file(inputFile);
+    ASSERT(file.is_open(), "Could not open file");
+
+    std::vector<std::string> out;
+    std::string line;
+    while (std::getline(file, line)) out.push_back(line);
+
+    file.close();
+    return out;
+}
